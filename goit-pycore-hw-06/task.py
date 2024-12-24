eebf9b92937f869c03751c7ab9e4693b78c73358
Vladyslav Mykhalytskyi <vmykhali666@gmail.com>
2024-12-24 23:01:35 +0200

    goit-pycore-hw-06

---
+++ goit-pycore-hw-06/task.py
@@ -0,0 +1,69 @@
+from collections import UserDict
+import re
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if self.validate(value):
+            super().__init__(value)
+        else:
+            raise ValueError("Неправильний формат телефону. Повинно бути 10 цифр.")
+
+    @staticmethod
+    def validate(value):
+        return bool(re.fullmatch(r"\d{10}", value))
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return True
+        return False
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                return True
+        return False
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones = "; ".join(phone.value for phone in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            return True
+        return False
