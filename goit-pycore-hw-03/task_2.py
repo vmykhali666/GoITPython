132dd7b4c6de38a85704482ed707e4b8790f87ac
Vladyslav Mykhalytskyi <vmykhali666@gmail.com>
2024-12-26 20:59:33 +0200

    fix when quantity bigger max count numbers

--- goit-pycore-hw-03/task_2.py
+++ goit-pycore-hw-03/task_2.py
@@ -1,8 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if not (1 <= min < max <= 1000) or not (min <= quantity <= max):
+    if not (1 <= min < max <= 1000) or not (1 <= quantity <= (max - min + 1)):
         return []
-    return sorted(random.sample(range(min, max + 1), quantity))
+    
+    unique_numbers = random.sample(range(min, max + 1), quantity)
+    
+    return sorted(unique_numbers)
 
-print(get_numbers_ticket(1, 49, 6))
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+print(get_numbers_ticket(10, 11, 11))
